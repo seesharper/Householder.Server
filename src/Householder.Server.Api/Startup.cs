8f7d8973b8c07e256148a5032fdcc8083677afc4
Eirik <eirikzh@gmail.com>
2020-08-21 16:46:54 +0200

    h

--- src/Householder.Server.Api/Startup.cs
+++ src/Householder.Server.Api/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Pomelo.EntityFrameworkCore.MySql;
 
 namespace Householder.Server.Api
 {
@@ -27,6 +23,8 @@ namespace Householder.Server.Api
         {
             // TODO: Use LightInject
             services.AddControllers();
+            services.AddDbContext<DbContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
