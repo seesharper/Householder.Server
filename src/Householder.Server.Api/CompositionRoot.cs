b6e332d4c419092d2edb11b3857d09eea4fa72af
Eirik Hemstad <eirikzh@gmail.com>
2020-08-31 09:50:50 +0200

    Added some services to container

--- src/Householder.Server.Api/CompositionRoot.cs
+++ src/Householder.Server.Api/CompositionRoot.cs
@@ -1,4 +1,9 @@
+using Householder.Server.Api.Controllers;
+using Householder.Server.DataAccess;
+using Householder.Server.Queries;
 using LightInject;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Householder.Server.Api
 {
@@ -6,7 +11,25 @@ namespace Householder.Server.Api
     {
         public void Compose(IServiceRegistry serviceRegistry)
         {
-            throw new System.NotImplementedException();
+            var configuration = GetConfiguration();
+            serviceRegistry.Register<IConfigurationRoot>(c => configuration);
+
+            serviceRegistry.Register<IMySqlDatabase>(c => new MySqlDatabase(configuration["ConnectionStrings:DefaultConnection"]));
+        
+            serviceRegistry.Register<IQueryProcessor>(c => new QueryProcessor(null));
+        
+            // Register controllers
+            serviceRegistry.Register<ResidentController>(c => new ResidentController(c.GetInstance<IQueryProcessor>(), null));
+        }
+
+        private IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .AddJsonFile("appsettings.json", true);
+
+            return builder.Build();
         }
     }
 }
